4f48f887d6eba6c9edde62df2364940813c9da1f
doug <linderd@iinet.net.au>
2015-03-16 11:07:33 +0800

    adds a test to make sure the macro does actually work

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,11 @@
 #[macro_export]
 macro_rules! fmt(($token:expr) => (format!("{:?}", $token)));
 
+#[cfg(test)]
+mod test {
+
+  #[test]
+  fn test_macro_works_at_all() {
+    let _ = fmt!(100);
+  }
+}
